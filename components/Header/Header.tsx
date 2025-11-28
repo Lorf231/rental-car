7f08f1cb38ba61d6656a19476c8ade6d178a8d03
Roman Fliunt <romanflyn@gmail.com>
2025-11-28 03:16:47 +0200

    added html to hero and header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -6,6 +6,17 @@ import React from "react";
 const Header = () => {
     return (
         <>
+        <Link href="/">Rental<span>Car</span></Link>
+        <nav>
+            <ul>
+                <li>
+                    <Link href="/">Home</Link>
+                </li>
+                <li>
+                    <Link href="/about">Catalog</Link>
+                </li>
+            </ul>
+        </nav>
         </>
     )
 }
--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -2,8 +2,17 @@ import Link from "next/link";
 
 const Hero = () => {
     return (
-        <>
-        </>
+    <section>
+      <div>
+        <div>
+          <h1>Find your perfect rental car</h1>
+          <p>
+            Reliable and budget-friendly rentals for any journey
+          </p>
+        </div>
+        <Link href="/catalog">View Catalog</Link>
+      </div>
+    </section>
     )
 }
 
