4688f5eb02e3aa7215a9d28bf24532ce62503ee0
Roman Fliunt <romanflyn@gmail.com>
2025-11-28 03:07:24 +0200

    added files for home page

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import Link from "next/link";
+
+const Footer = () => {
+    return (
+        <>
+        </>
+    )
+}
+
+export default Footer;
---
+++ components/Header/Header.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import Link from "next/link";
+import React from "react";
+
+const Header = () => {
+    return (
+        <>
+        </>
+    )
+}
+
+export default Header;
